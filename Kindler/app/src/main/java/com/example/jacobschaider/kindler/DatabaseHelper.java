390ee61ccc3f69cd23491754f5e1ef6d54ba910a
Natalie Pearson <pearsonfam1@guest-wireless-upc-1606-10-120-099-001.usc.edu>
2018-10-17 11:07:06 -0700

    nothing

--- Kindler/app/src/main/java/com/example/jacobschaider/kindler/DatabaseHelper.java
+++ Kindler/app/src/main/java/com/example/jacobschaider/kindler/DatabaseHelper.java
@@ -7,14 +7,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.util.ArrayList;
 
 public class DatabaseHelper {
-    public FirebaseAuth fb;
-    public DatabaseHelper(FirebaseAuth fb) {
-        this.fb = fb;
-    }
 
-    public ArrayList<Post> getPostsOfUser() {
-        ArrayList<Post> listOfPosts = new ArrayList<Post>();
-        //interact with database
-        return listOfPosts;
-    }
+
+
 }
